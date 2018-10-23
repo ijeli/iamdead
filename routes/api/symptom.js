c836622a84584cb305ffc38d46051e819b535b9f
Yaraye <yaraye@gmail.com>
2018-10-22 21:28:44 -0400

    updates

--- routes/api/symptom.js
+++ routes/api/symptom.js
@@ -4,3 +4,5 @@ const profileController = require('../../controller/profileController')
 module.exports = router;
 
 router.route('/').get(profileController.findAllSymptoms);
+router.route("/:id").get(profileController.findOneSymptoms);
+ 
